756b02dfba81fc6d6aa83e3ff2587ff768d86783
anirudtate <anirud.programming@gmail.com>
2023-08-21 18:59:07 +0530

    added header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,31 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="nav-bar-wrapper">
+	<nav class="container">
+		<ul>
+			<li>
+				<strong style="font-size: 1.5rem;"
+					>form<span style="color: var(--pico-primary);">Thing</span></strong
+				>
+			</li>
+		</ul>
+		<ul>
+			<li><a href="asdf">about</a></li>
+		</ul>
+	</nav>
+</div>
+
+<div class="container">
+	<slot />
+</div>
+
+<style>
+	.nav-bar-wrapper {
+		position: sticky;
+		top: 0;
+		left: 0;
+		border-bottom: 1px solid var(--pico-muted-border-color);
+		background-color: var(--pico-background-color);
+	}
+</style>
