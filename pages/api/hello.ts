ad83954060f7e542eeb0baecdc44d5bb713eabec
anirudtate <anirud.programming@gmail.com>
2023-09-12 16:24:31 +0530

    drizzle setup done

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
+import { test } from "@/database/schema"
+
+import { db } from "@/lib/db"
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data[]>
 ) {
-  res.status(200).json({ name: "John Doe" })
+  const query = await db.select().from(test)
+  res.status(200).json(query)
 }
