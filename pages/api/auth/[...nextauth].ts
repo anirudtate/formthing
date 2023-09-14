1b2135bdeff7823be45f24f1f6ae9e9f32b7602e
anirudtate <anirud.programming@gmail.com>
2023-09-14 17:32:41 +0530

    added auth to db

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,14 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import * as dotenv from "dotenv"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { db } from "@/lib/db"
+
 dotenv.config()
 
 export const authOptions = {
+  adapter: DrizzleAdapter(db),
   pages: {
     signIn: "/auth/signin",
     signOut: "/auth/signout",
